7aa488fdf0430a6d583e632ec1937997d79b2bc7
Milap <milapprajapati707070@gmail.com>
2024-06-26 22:21:26 -0400

    Second Commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>My Name is Milap Prajapati</h1>
+        <h1>My Name is Milap P</h1>
       </header>
     </div>
   );
